da317558d8140c6f9306765d07ad8e834e87dcfe
Mawry <mau.stalcar@proton.me>
2026-05-15 11:16:27 -0300

    added REST API

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,19 @@ const express = require('express');
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-    res.send("you got 5 notes");
+    res.status(200).send("you got 5 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({ message: "note created" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "note updated" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "note deleted" });
 });
 
 app.listen(5001, () => {
